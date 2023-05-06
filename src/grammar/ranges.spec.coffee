c90be6b761ffa55b22bdc9e3ae82d222981e00e5
kiruse <26940742+Kiruse@users.noreply.github.com>
2023-05-07 01:25:08 +0200

    improve ranges algorithms

--- src/grammar/ranges.spec.coffee
+++ src/grammar/ranges.spec.coffee
@@ -1,8 +1,6 @@
 {CharCodeRanges, CharCodeRange} = require './ranges'
 {expect} = require 'chai'
 
-describe 'CharCodeRanges', ->
-
 describe 'CharCodeRange', ->
   it 'constructs', ->
     expect(CharCodeRange('a')).to.include
@@ -12,3 +10,73 @@ describe 'CharCodeRange', ->
       begin: 97
       end:   99
     expect(=> CharCodeRange('c', 'a')).to.throw()
+  it 'includes', ->
+    expect(CharCodeRange('a', 'c').includes('a')).to.be.true
+    expect(CharCodeRange('a', 'c').includes('b')).to.be.true
+    expect(CharCodeRange('a', 'c').includes('c')).to.be.true
+    expect(CharCodeRange('a', 'c').includes('d')).to.be.false
+  it 'overlaps', ->
+    expect(CharCodeRange('a', 'c').overlaps(CharCodeRange('b', 'd'))).to.be.true
+    expect(CharCodeRange('a', 'c').overlaps(CharCodeRange('c', 'd'))).to.be.true
+    expect(CharCodeRange('a', 'c').overlaps(CharCodeRange('d', 'e'))).to.be.false
+
+describe 'CharCodeRanges', ->
+  it 'constructs', ->
+    expect(CharCodeRanges([['a', 'c']])).to.lookLike
+      subranges: [
+        begin: 97
+        end:   99
+      ]
+    expect(CharCodeRanges([['a', 'c'], ['e', 'f']])).to.lookLike
+      subranges: [
+        begin: 97
+        end:   99
+      ,
+        begin: 101
+        end:   102
+      ]
+  it 'normalizes', ->
+    ranges = CharCodeRanges([['e', 'k'], ['a', 'c']])
+    expect(ranges.subranges[0]).to.include
+      begin: 97
+      end:   99
+    expect(ranges.subranges[1]).to.include
+      begin: 101
+      end:   107
+    
+    ranges = CharCodeRanges([['e', 'k'], ['f', 'h']])
+    expect(ranges.subranges).to.be.lengthOf 1
+    expect(ranges.subranges[0]).to.include
+      begin: 101
+      end:   107
+    
+    ranges = CharCodeRanges([['a', 'c'], ['d', 'f']])
+    expect(ranges.subranges).to.be.lengthOf 1
+    expect(ranges.subranges[0]).to.include
+      begin: 97
+      end:   102
+  it 'produces unique ranges', ->
+    r1 = CharCodeRanges([['a', 'c'], ['e', 'f']])
+    r2 = CharCodeRanges([['c', 'e']])
+    [r1diff, rUnion, r2diff] = r1.unique(r2)
+    expect(r1diff.subranges).to.be.lengthOf 2
+    expect(r1diff.subranges).to.lookLike [
+      begin: 97
+      end:   98
+    ,
+      begin: 102
+      end:   102
+    ]
+    expect(r2diff.subranges).to.be.lengthOf 1
+    expect(r2diff.subranges).to.lookLike [
+      begin: 100
+      end:   100
+    ]
+    expect(rUnion.subranges).to.be.lengthOf 2
+    expect(rUnion.subranges).to.lookLike [
+      begin: 99
+      end:   99
+    ,
+      begin: 101
+      end:   101
+    ]
--- src/grammar/ranges.ts
+++ src/grammar/ranges.ts
@@ -1,6 +1,9 @@
 export interface CharCodeRanges {
   subranges: CharCodeRange[];
   includes(char: string): boolean;
+  union(other: CharCodeRanges): CharCodeRanges;
+  intersection(other: CharCodeRanges): CharCodeRanges;
+  difference(other: CharCodeRanges): CharCodeRanges;
   /** Produce unique `CharCodeRanges` given a counterpart, i.e. both ranges' intersections and differences.
    * 
    * @returns `[thisDifference, intersection, otherDifference]`
@@ -9,50 +12,71 @@ export interface CharCodeRanges {
   normalize(): this;
 }
 
-export const CharCodeRanges = (subranges: (CharCodeRange | [number, number])[]): CharCodeRanges => ({
-  subranges: normalizeSubrangesArg(subranges),
-  includes(char: string) {
-    if (char.length !== 1) throw Error('Expected single character');
-    for (const subrange of this.subranges) {
-      if (subrange.includes(char)) return true;
-    }
-    return false;
-  },
-  unique(other: CharCodeRanges) {
-    const thisDiff  = CharCodeRanges([]);
-    const intersect = CharCodeRanges([]);
-    const otherDiff = CharCodeRanges([]);
-    
-    for (const thisSubrange of this.subranges) {
-      for (const otherSubrange of other.subranges) {
-        if (thisSubrange.overlaps(otherSubrange)) {
-          if (thisSubrange.begin < otherSubrange.begin)
-            thisDiff.subranges.push(CharCodeRange(thisSubrange.begin, otherSubrange.begin-1));
-          if (thisSubrange.end > otherSubrange.end)
-            thisDiff.subranges.push(CharCodeRange(otherSubrange.end+1, thisSubrange.end));
-          intersect.subranges.push(CharCodeRange(Math.max(thisSubrange.begin, otherSubrange.begin), Math.min(thisSubrange.end, otherSubrange.end)));
-        } else {
-          thisDiff.subranges.push(thisSubrange);
+export const CharCodeRanges = (subranges: (CharCodeRange | [string | number, string | number])[]): CharCodeRanges => {
+  const result: CharCodeRanges = {
+    subranges: normalizeSubrangesArg(subranges),
+    includes(char: string) {
+      if (char.length !== 1) throw Error('Expected single character');
+      for (const subrange of this.subranges) {
+        if (subrange.includes(char)) return true;
+      }
+      return false;
+    },
+    union(other: CharCodeRanges) {
+      return CharCodeRanges([...this.subranges, ...other.subranges]).normalize();
+    },
+    intersection(other: CharCodeRanges) {
+      const result = CharCodeRanges([]);
+      for (const thisSubrange of this.subranges) {
+        for (const otherSubrange of other.subranges) {
+          if (thisSubrange.overlaps(otherSubrange)) {
+            result.subranges.push(CharCodeRange(Math.max(thisSubrange.begin, otherSubrange.begin), Math.min(thisSubrange.end, otherSubrange.end)));
+          }
         }
       }
-    }
-    
-    return [thisDiff.normalize(), intersect.normalize(), otherDiff.normalize()];
-  },
-  // this normalization differs from the argument normalization: it sorts & merges overlapping ranges
-  normalize() {
-    const ranges = this.subranges.sort((a, b) => a.begin - b.begin);
-    for (let i = 0; i < ranges.length; ++i) {
-      if (ranges[i+1].begin <= ranges[i].end+1) {
-        const newRange = CharCodeRange(ranges[i].begin, Math.max(ranges[i].end, ranges[i+1].end));
-        ranges.splice(i, 2, newRange);
-        --i;
+      return result.normalize();
+    },
+    difference(other: CharCodeRanges) {
+      const result = CharCodeRanges([]);
+      for (const thisSubrange of this.subranges) {
+        let subrange = CharCodeRange(thisSubrange.begin, thisSubrange.end);
+        for (const otherSubrange of other.subranges) {
+          if (subrange.begin >= otherSubrange.begin && subrange.end <= otherSubrange.end) {
+            subrange.begin = 0;
+            subrange.end = -1;
+            break;
+          }
+          if (subrange.begin >= otherSubrange.begin && subrange.begin <= otherSubrange.end) {
+            subrange.begin = otherSubrange.end+1;
+          }
+          if (subrange.end >= otherSubrange.begin && subrange.end <= otherSubrange.end) {
+            subrange.end = otherSubrange.begin-1;
+          }
+        }
+        result.subranges.push(subrange);
       }
-    }
-    this.subranges = ranges;
-    return this;
-  },
-});
+      return result.normalize();
+    },
+    unique(other: CharCodeRanges) {
+      return [this.difference(other), this.intersection(other), other.difference(this)]
+    },
+    // this normalization differs from the argument normalization: it sorts & merges overlapping ranges, and removes empty/negative ranges
+    normalize() {
+      const ranges = this.subranges.filter(sr => sr.begin <= sr.end).sort((a, b) => a.begin - b.begin);
+      for (let i = 0; i < ranges.length-1; ++i) {
+        if (ranges[i+1].begin <= ranges[i].end+1) {
+          const newRange = CharCodeRange(ranges[i].begin, Math.max(ranges[i].end, ranges[i+1].end));
+          ranges.splice(i, 2, newRange);
+          --i;
+        }
+      }
+      this.subranges = ranges;
+      return this;
+    },
+  };
+  result.normalize();
+  return result;
+}
 
 /** Range of char codes, compatible with @unicode/unicode-x.x.x packages */
 export interface CharCodeRange {
@@ -78,10 +102,8 @@ export const CharCodeRange = (begin: number | string, end = begin) => {
   };
 }
 
-function normalizeSubrangesArg(subranges: (CharCodeRange | [number, number])[]): CharCodeRange[] {
-  return subranges
-    .map(sr => Array.isArray(sr) ? CharCodeRange(sr[0], sr[1]) : sr)
-    .sort((a, b) => a.begin - b.begin);
+function normalizeSubrangesArg(subranges: (CharCodeRange | [string | number, string | number])[]): CharCodeRange[] {
+  return subranges.map(sr => Array.isArray(sr) ? CharCodeRange(sr[0], sr[1]) : sr);
 }
 
 function normalizeRangeArg(arg: string | number): number {
